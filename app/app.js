920fae2ead51f46237e833a79168ca7665970cfa
AsianDave94 <Davidtjmalmberg@gmail.com>
2018-05-08 12:34:22 +0200

    stylesheet done

--- app/app.js
+++ app/app.js
@@ -2,11 +2,12 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
+var path = require('path')
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
 app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.locals.siteTitle = 'Noise level supervisor';
 app.locals.allSpeakers = dataFile.speakers;
