4ab4f283619283a5cb2122a324df319a56f2791d
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-05-16 10:01:48 +0200

    ?

--- app/public/js/mytemprature.js
+++ app/public/js/mytemprature.js
@@ -12,9 +12,23 @@ $(function() {
         function showMeasurements(data) {
      output += '<h1>Previous Measurements</h1>';
      $.each(data,function(key, item) {
-         output += '<h2>'+ 'Id '+ item.Id +'</h2>'
-        output += '<h1> '+ 'Dato ' + item.Date + '</h1>';
-          output += '<h3> ' + item.Temperature + 'C' + '</h3>';
+        <ul data-role="listview" class="" id="list">
+
+        <li id="A"><a href="#page2" data-ajax="false">A</a></li>
+        <li id="B"><a href="#page2" data-ajax="false">B</a></li>
+        <li id="C"><a href="#page2" data-ajax="false">C</a></li>
+        <li id="D"><a href="#page2" data-ajax="false">D</a></li>
+        </ul>
+         output += '<h2 onClick = MyDeleteTemperature>'+ 'Id '+ item.Id +'</h2>'
+         output += '<h1> '+ 'Dato ' + item.Date + '</h1>';
+
+         if (item.Temperature > 25) {
+            output += '<h3> ' + item.Temperature + 'C' + '!' + '</h3>';
+         }
+
+         output += '<h3> ' + item.Temperature + 'C' + '</h3>';
+
+         
         window.location.reload(10);
     },5000);
     $('.Measurements').html(output);
