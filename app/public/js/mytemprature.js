c941bfd76c5bd30b8f39fbcf4e8fbe54b29bf94f
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-05-18 10:13:17 +0200

    Delete works

--- app/public/js/mytemprature.js
+++ app/public/js/mytemprature.js
@@ -1,59 +1,60 @@
-$(function() {
+$(function () {
 
     var output = '';
 
     $.ajax({
-        type : 'GET',
+        type: 'GET',
         dataType: 'jsonp',
         url: 'http://localhost:55168/Service1.svc/getallmeasurments/',
         success: showMeasurements
-        });
-       
-        $( '.deletemeasurment').on('click', function(e){
-            if(e.target.className == 'glyphicon glyphicon-remove'){
-                var deleteurl = 'http://localhost:55168/Service1.svc/delete/' + e.target.id
-        
-                $.ajax({
-                    type : 'delete',
-                    url : deleteurl,
-                    success : deletemeasurment,
-                    dataType : 'json',
-                    crossDomain : true
-                });
-            }
-        function deletemeasurment(data){
-             $.getJSON('http://localhost:55168/Service1.svc/getallmeasurments/', showMeasurements);
-             
-        }
-        });
+    });
 
-        function showMeasurements(data) {
-            output += '<div class = "deletemeasurment">'; 
-            output += '<tr>';
-            output += '<th>ID</th>';
-            output += '<th>DATE</th>';
-            output += '<th>TEMPERATURE</th>';
-            output += '</tr>';
-     $.each(data,function(key, item) {
-    
+    $(document).on("click", '.feedback-delete', function (e) {
+        $('#wrapper').append($('<p>fwggw</p>'));
+        // if (e.target.className == 'glyphicon glyphicon-remove') {
+        var deleteurl = 'http://localhost:55168/Service1.svc/delete/' + e.target.id
+
+        $.ajax({
+            type: 'delete',
+            url: deleteurl,
+            success: deletemeasurment,
+            dataType: 'json',
+            crossDomain: true
+        });
+        // }
+        function deletemeasurment(data) {
+            $.getJSON('http://localhost:55168/Service1.svc/getallmeasurments/', showMeasurements);
+
+        }
+    });
+
+    function showMeasurements(data) {
+
         output += '<tr>';
-        output += '<th>' + item.Id + '</th>';
-        output += '<th>' + item.Date + '</th>';
-      
-        if (item.Temperature > 26) {
-            output += '<th>' + item.Temperature + "!" + '</th>';
-           
-        }
-        else{
-             output += '<th>' + item.Temperature + '</th>';
-        }
-        output += '<th> <div class="media-left"><button class="feedback-delete btn btn-xs btn-danger"><span id="' + item.Id + '" class="glyphicon glyphicon-remove"></span></button></div> </th>';
+        output += '<th>ID</th>';
+        output += '<th>DATE</th>';
+        output += '<th>TEMPERATURE</th>';
         output += '</tr>';
-        output += '</div>';
-        
-     });
+        $.each(data, function (key, item) {
 
-    $('.temperatures').html(output);
-}
+            output += '<tr>';
+            output += '<th>' + item.Id + '</th>';
+            output += '<th>' + item.Date + '</th>';
+
+            if (item.Temperature > 26) {
+                output += '<th>' + item.Temperature + "!" + '</th>';
+
+            }
+            else {
+                output += '<th>' + item.Temperature + '</th>';
+            }
+            output += `<th> <div class="media-left"><button id='${item.Id}' class="feedback-delete btn btn-xs btn-danger"><span id="' + item.Id + '" class="glyphicon glyphicon-remove"></span></button></div> </th>`;
+            output += '</tr>';
+
+
+        });
+
+        $('.temperatures').html(output);
+    }
 
 });
