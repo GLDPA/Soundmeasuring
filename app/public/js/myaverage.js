15cef72a223fa193c4a12c55b1f89625f688fab7
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-05-18 14:09:56 +0200

    vis average i /average

---
+++ app/public/js/myaverage.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    var output = '';
+
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        url: 'http://localhost:55168/Service1.svc/getaverage/',
+        success: showAverage
+    });
+
+    function showAverage(data) {
+        output += '<h2> Average </h2>';
+        output += '<p>' + data + '</p>';
+
+
+    }
+
+    $('.average').html(output);
+
+
+});
